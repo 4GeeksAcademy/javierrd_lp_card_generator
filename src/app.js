b62ef0897140ab471b4c991e3a2d199c7b4a5869
Javier Rodríguez <134724418+javierrdlp@users.noreply.github.com>
2024-11-06 10:18:26 +0000

    Primera subida, ejercicio completo con extras

--- src/app.js
+++ src/app.js
@@ -6,6 +6,53 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //write your code here ♦ ♥ ♠ ♣
+  createCard();
+  let buttonCreate = document.querySelector("#generador");
+  buttonCreate.addEventListener("click", function(event) {
+    createCard();
+  });
+  let buttonSize = document.querySelector("#size");
+  let height = document.querySelector("#height-number");
+  let width = document.querySelector("#width-number");
+  let card = document.querySelector(".card");
+  console.log(height.innerHTML);
+
+  buttonSize.addEventListener("click", function(event) {
+    console.log(height.value);
+    if (height.value > 299 && width.value > 299) {
+      card.style.height = `${height.value}px`;
+      card.style.width = `${width.value}px`;
+      createCard();
+    }
+  });
+  setInterval(createCard, 10000);
 };
+
+function createCard() {
+  let randomNumber = Math.floor(Math.random() * 13);
+  let randomSuit = Math.floor(Math.random() * 4);
+  let numberArray = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let suitArray = ["♦", "♥", "♠", "♣"];
+
+  let number = document.querySelector(".number");
+  number.innerHTML = numberArray[randomNumber];
+
+  let suitTop = document.querySelector(".suitTop");
+  let suitBottom = document.querySelector(".suitBottom");
+
+  let suit = suitArray[randomSuit];
+
+  let paragraphs = document.querySelectorAll('[class*="suit"]');
+
+  if (randomSuit <= 1) {
+    paragraphs[0].style.color = "red";
+    paragraphs[1].style.color = "red";
+  } else {
+    paragraphs[0].style.color = "black";
+    paragraphs[1].style.color = "black";
+  }
+
+  suitTop.innerHTML = suit;
+  suitBottom.innerHTML = suit;
+}
